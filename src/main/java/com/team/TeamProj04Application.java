44e1069b8e02ce32f91b5c04498b7fa6c0c670bf
Yeongsik Jeong <fkdlemgoej@gmail.com>
2023-03-23 10:14:08 +0900

    init - 2023.03.23 - jys

---
+++ src/main/java/com/team/TeamProj04Application.java
@@ -0,0 +1,13 @@
+package com.team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamProj04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamProj04Application.class, args);
+	}
+
+}
